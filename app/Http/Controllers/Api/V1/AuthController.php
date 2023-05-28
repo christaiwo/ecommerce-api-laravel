ab8040f301091592ea15707df25d912d517d856e
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-28 20:05:16 +0100

    Added Login and Registration Controller

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\LoginRequest;
+use App\Http\Requests\V1\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * get the request and store the user in the database.
+     */
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+        
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        /** @var \App\Models\User $user */ 
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response()->json(compact('user', 'token'), 200);
+    }
+
+    /**
+     * get the request and login user
+     */
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        if(!Auth::attempt($data)){
+            return response()->json([
+                'message' => 'Provided email address or password is incorrect'
+            ], 401);
+        }
+
+        /** @var \App\Models\User $user */ 
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response()->json(compact('user', 'token'), 200);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function logout()
+    {
+        //
+    }
+
+}
