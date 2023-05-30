2a015796da4fd00d315984b3dbe8085f94c28463
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-30 09:43:06 +0100

    Update and address field to orders

--- database/migrations/2023_05_29_084136_create_orders_table.php
+++ database/migrations/2023_05_29_084136_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onUpdate('restrict')->onDelete('cascade');
+            $table->foreignId('address_id')->constrained()->onUpdate('restrict')->onDelete('cascade');
             $table->string('hash')->unique();
             $table->string('amount')->default(0);
             $table->string('payment_method')->nullable();
