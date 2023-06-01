8c7aa2a1f6f14273c99d33c4484f90a92186ace6
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-06-01 12:55:02 +0100

    update user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name')->nullable();
             $table->string('email')->unique();
+            $table->integer('is_admin')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
